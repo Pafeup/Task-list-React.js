e2ed64e12d63cfbfe02d19f7f140e68067408914
Paweł Pełka <pinio.pelka@gmail.com>
2023-07-03 15:03:42 +0200

    if input is empty don't add new task

--- src/Form/index.js
+++ src/Form/index.js
@@ -1,19 +1,25 @@
 import { useState } from "react";
 import "./style.css";
 
-const Form = ({addNewTask}) => {
+const Form = ({ addNewTask }) => {
   const [newTaskContent, setNewTaskContent] = useState("");
+
+  const trimmedNewTaskContent = newTaskContent.trim()
 
   const onFromSubmit = (event) => {
     event.preventDefault();
-    addNewTask(newTaskContent.trim());
-  }
+    if (trimmedNewTaskContent !== "") {
+      addNewTask(trimmedNewTaskContent);
+      setNewTaskContent("")
+    };
+
+  };
 
   return (
     <form className="form" onSubmit={onFromSubmit}>
       <input
         value={newTaskContent}
-        onChange={({target}) => setNewTaskContent(target.value)}
+        onChange={({ target }) => setNewTaskContent(target.value)}
         className="form__input newTask"
         type="text"
         placeholder="Co jest do zrobienia?"
