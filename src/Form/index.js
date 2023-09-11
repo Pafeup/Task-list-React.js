a66ec9e9ed0112d84510fca0e127d8924ab2a581
Paweł Pełka <pinio.pelka@gmail.com>
2023-09-11 14:52:52 +0200

    added focun on input

--- src/Form/index.js
+++ src/Form/index.js
@@ -1,10 +1,18 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
 import "./style.css";
 
+
+
 const Form = ({ addNewTask }) => {
+  const inputRef = useRef(null);
+
+  const inputFocus = () => {
+    inputRef.current.focus();
+  };
+
   const [newTaskContent, setNewTaskContent] = useState("");
 
-  const trimmedNewTaskContent = newTaskContent.trim()
+  const trimmedNewTaskContent = newTaskContent.trim();
 
   const onFromSubmit = (event) => {
     event.preventDefault();
@@ -18,13 +26,14 @@ const Form = ({ addNewTask }) => {
   return (
     <form className="form" onSubmit={onFromSubmit}>
       <input
+        ref={inputRef}
         value={newTaskContent}
         onChange={({ target }) => setNewTaskContent(target.value)}
         className="form__input newTask"
         type="text"
         placeholder="Co jest do zrobienia?"
       />
-      <button className="form__button addNewTask">Dodaj zadanie</button>
+      <button onClick={inputFocus} className="form__button addNewTask">Dodaj zadanie</button>
     </form>)
 }
 
