059d132f0d8bcc9dbf0e83429cddfde35a7fcd01
Paweł Pełka <pinio.pelka@gmail.com>
2023-11-16 14:29:33 +0100

    separating the useLocalStorage function into a separate hook

--- src/useTasks.js
+++ src/useTasks.js
@@ -1,16 +1,6 @@
-import { useEffect, useState } from "react";
+import { useLocalStorage } from "./useLocalStorage";
 
 const useTasks = () => {
-    function useLocalStorage(key, defaultValue) {
-        const storedValue = JSON.parse(localStorage.getItem(key));
-        const [value, setValue] = useState(storedValue || defaultValue);
-
-        useEffect(() => {
-            localStorage.setItem(key, JSON.stringify(value));
-        }, [key, value]);
-
-        return [value, setValue];
-    };
     const [hideDone, setHideDone] = useLocalStorage("hideDone", false);
     const [tasks, setTasks] = useLocalStorage("tasks", []);
 
